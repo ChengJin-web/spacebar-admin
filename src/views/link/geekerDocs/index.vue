9207b47d64b4a22250f437c0a872683697252f8f
chengjin <57589172@qq.com>
2023-09-11 14:48:46 +0800

    feat: 🚀 初始化项目

---
+++ src/views/link/geekerDocs/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card content-box">
+    <span class="text">
+      项目文档： <a href="https://docs.spacebar.top" target="_blank">https://docs.spacebar.top</a> 🍒🍉🍊
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts" name="spacebarDocs"></script>
+
+<style scoped lang="scss">
+@import "./index.scss";
+</style>
